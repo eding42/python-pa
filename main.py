08ed71cdd67824fc6929cf5b49cf970ce7981367
Edward Ding <eding2019@gmail.com>
2019-08-20 22:56:33 -0700

    Added preliminary support for WAV concatenation. Stitched two files together! Implemented the sox module and deleted extraneous files

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from pydub import AudioSegment
-from pydub.playback import play
+#from pydub import AudioSegment
+#from pydub.playback import play
 
-song = AudioSegment.from_wav("never_gonna_give_you_up.wav")
+#song = AudioSegment.from_wav("never_gonna_give_you_up.wav")
 
-sound = AudioSegment.from_file("mysound.wav", format="wav")
-play(sound)
+#sound = AudioSegment.from_file("mysound.wav", format="wav")
+#play(sound)
+
+import sox
+
+# create combiner
+cbn = sox.Combiner()
+
+# create the output file
+cbn.build(
+    ['./bin/aachen.wav', './bin/aaberg.wav'], 'output.wav', 'concatenate'
+)
+
